3aa1e606f929c9edc4e2dfe61059d79f27dd40ac
MinimalEffort07 <90430937+MinimalEffort07@users.noreply.github.com>
2024-11-10 20:19:20 +1100

    Black formatting (#1)

--- alacritty.py
+++ alacritty.py
@@ -2,45 +2,49 @@ from pygls.server import LanguageServer
 from lsprotocol import types
 import logging
 
-logging.basicConfig(filename='pygls.log', filemode='w', level=logging.DEBUG)
+logging.basicConfig(filename="pygls.log", filemode="w", level=logging.DEBUG)
 log = logging.getLogger(__name__)
 
-state = {"general": {
-                "import": [None],
-                "working_directory": ["'None'", None],
-                "live_config_reload": ["true", "false"],
-                "ipc_socket": ["true", "false"],
-            },
-         "env": [None],
-         "window": {
-             "dimensions": {
-                 "columns": [None],
-                 "lines": [None],
-                 }
-             }
-         }
+state = {
+    "general": {
+        "import": [None],
+        "working_directory": ["'None'", None],
+        "live_config_reload": ["true", "false"],
+        "ipc_socket": ["true", "false"],
+    },
+    "env": [None],
+    "window": {
+        "dimensions": {
+            "columns": [None],
+            "lines": [None],
+        }
+    },
+}
 
 server = LanguageServer("Alacritty-LanguageServer", "v0.1")
+
 
 def get_section(params, document):
 
     line = params.position.line
 
-    while(line >= 0 and document.lines[line][0] != "["):
+    while line >= 0 and document.lines[line][0] != "[":
         log.debug(f"Not the section line: {line}: {document.lines[line]}")
         line -= 1
 
     if line < 0:
-        log.error("Reached beginning of file looking for section line but couldn't find it")
+        log.error(
+            "Reached beginning of file looking for section line but couldn't find it"
+        )
         return None
 
     # Do two strips, the first targeting whitespace and the second to target the toml square
     # brackets. Could do it in one but don't want to have to deal with all the different whitespace
     # characters that could be at the end of the line e.g. ^M on windows.
-    section = document.lines[line].strip().strip('[]')
+    section = document.lines[line].strip().strip("[]")
     log.debug(f"Section line: {section} , on line number: {line}")
 
-    section_components = section.split('.')
+    section_components = section.split(".")
     log.debug(f"components: {section_components}")
 
     dictionary_depth = len(section_components)
@@ -50,7 +54,7 @@ def get_section(params, document):
     sub_section = state
 
     try:
-        while(level < dictionary_depth):
+        while level < dictionary_depth:
             sub_section = sub_section[section_components[level]]
             log.debug(f"sub_section: {sub_section}")
             level += 1
@@ -61,7 +65,10 @@ def get_section(params, document):
 
     return sub_section
 
-@server.feature(types.TEXT_DOCUMENT_COMPLETION, types.CompletionOptions(trigger_characters=["="]))
+
+@server.feature(
+    types.TEXT_DOCUMENT_COMPLETION, types.CompletionOptions(trigger_characters=["="])
+)
 def completions(params: types.CompletionParams):
     document = server.workspace.get_document(params.text_document.uri)
     current_line = document.lines[params.position.line].strip()
@@ -85,10 +92,12 @@ def completions(params: types.CompletionParams):
 
         if type(options) == type([]):
             log.debug("Leaf key")
-            return [types.CompletionItem(label=opt) for opt in options if None != opt ]
+            return [types.CompletionItem(label=opt) for opt in options if None != opt]
         elif type(options) == type({}):
             log.debug("non leaf key")
-            return [types.CompletionItem(label=opt) for opt in options.keys() if None != opt ]
+            return [
+                types.CompletionItem(label=opt) for opt in options.keys() if None != opt
+            ]
         else:
             log.debug("what")
     except KeyError as e:
@@ -99,6 +108,7 @@ def completions(params: types.CompletionParams):
         return []
 
     # return [types.CompletionItem(label="lool"), types.CompletionItem(label="sick")]
+
 
 if __name__ == "__main__":
     server.start_io()
