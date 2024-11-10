d1faabad03f38fe8b4ee0904733d5933090ea863
MinimalEffort07 <90430937+MinimalEffort07@users.noreply.github.com>
2024-11-10 20:18:48 +1100

    Added partial word completion

--- alacritty.py
+++ alacritty.py
@@ -5,7 +5,21 @@ import logging
 logging.basicConfig(filename='pygls.log', filemode='w', level=logging.DEBUG)
 log = logging.getLogger(__name__)
 
-state = {"general": {"import": [None], "working_directory": ["'None'", None], "live_config_reload": ["true", "false"], "ipc_socket": ["true", "false"]}}
+state = {"general": {
+                "import": [None],
+                "working_directory": ["'None'", None],
+                "live_config_reload": ["true", "false"],
+                "ipc_socket": ["true", "false"],
+            },
+         "env": [None],
+         "window": {
+             "dimensions": {
+                 "columns": [None],
+                 "lines": [None],
+                 }
+             }
+         }
+
 server = LanguageServer("Alacritty-LanguageServer", "v0.1")
 
 def get_section(params, document):
@@ -15,12 +29,12 @@ def get_section(params, document):
     while(line >= 0 and document.lines[line][0] != "["):
         log.debug(f"Not the section line: {line}: {document.lines[line]}")
         line -= 1
- 
+
     if line < 0:
         log.error("Reached beginning of file looking for section line but couldn't find it")
         return None
 
-    # Do two strips, the first targeting whitespace and the second to target the toml square 
+    # Do two strips, the first targeting whitespace and the second to target the toml square
     # brackets. Could do it in one but don't want to have to deal with all the different whitespace
     # characters that could be at the end of the line e.g. ^M on windows.
     section = document.lines[line].strip().strip('[]')
@@ -34,44 +48,54 @@ def get_section(params, document):
 
     level = 0
     sub_section = state
+
     try:
         while(level < dictionary_depth):
             sub_section = sub_section[section_components[level]]
             log.debug(f"sub_section: {sub_section}")
             level += 1
-    except:
-        log.error(f"Syntax error in section definition: {'.'.join(section_components)}")
-        return None
+    except KeyError:
+        raise KeyError(f"Minor mode doesn't exist")
+    except IndexError:
+        raise IndexError(f"Minor mode level doens't exist")
 
     return sub_section
-    
+
 @server.feature(types.TEXT_DOCUMENT_COMPLETION, types.CompletionOptions(trigger_characters=["="]))
 def completions(params: types.CompletionParams):
     document = server.workspace.get_document(params.text_document.uri)
     current_line = document.lines[params.position.line].strip()
 
     current_line_components = current_line.split()
-    key = current_line_components[0] if len(current_line_components) > 0 else None
-    if None == key:
-        log.debug("Key is None")
-    else:
-        log.debug(f"Completing for key: {key}")
-
-    options = get_section(params, document)
-
-    labels = options[key] if key != None else options
+    curr_text = current_line_components[0] if len(current_line_components) > 0 else None
 
     try:
-        if type(labels) == type([]):
+        section = get_section(params, document)
+        options = section
+        # if current line isn't empty
+        if None != curr_text:
+            # If current text doesn't match a valid minor section key then assume curr_text
+            # is unfinished and instead of display values for some key, don't index into a
+            # key and match against the keys themsleves
+            if curr_text not in section.keys():
+                options = section
+            else:
+                # curr_text matched a key, provide completion for that keys values
+                options = section[curr_text]
+
+        if type(options) == type([]):
             log.debug("Leaf key")
-            return [types.CompletionItem(label=opt) for opt in labels if None != opt ] 
-        elif type(labels) == type({}):
+            return [types.CompletionItem(label=opt) for opt in options if None != opt ]
+        elif type(options) == type({}):
             log.debug("non leaf key")
-            return [types.CompletionItem(label=opt) for opt in labels.keys() if None != opt ] 
-        else:   
+            return [types.CompletionItem(label=opt) for opt in options.keys() if None != opt ]
+        else:
             log.debug("what")
-    except:
-        log.debug(f"Couldn't get labels")       
+    except KeyError as e:
+        log.error(e)
+        return []
+    except IndexError as e:
+        log.error(e)
         return []
 
     # return [types.CompletionItem(label="lool"), types.CompletionItem(label="sick")]
